d397a3e36715fc0b5de59d0dc83acad5637f9b9c
1155083040 <1155083040@link.cuhk.edu.hk>
2019-03-22 01:45:29 +0800

    added crawlpage.py and html in user; demo py in rootDir

---
+++ portal/crawlpage.py
@@ -0,0 +1,30 @@
+#need to be added into view.py
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Category, Message
+from django.contrib.auth.models import User
+from django.views.decorators import csrf
+
+# import bs4 lib
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def crawlpage(request):
+	content = {}
+	if request.user.is_authenticated:
+	#logic for crawling
+		html = urlopen({{crawllink}}).read().decode('utf-8') 
+		soup = BeautifulSoup(html ,'html.parser')
+		element = soup.select({{crawltag}})
+
+	# hardcode version:
+	# html = urlopen("http://course.cse.cuhk.edu.hk/~csci3100/").read().decode('utf-8')
+	# soup = BeautifulSoup(html ,'html.parser')
+	# print_text = soup.select('br+ p')
+	# print(print_text)
+
+	#crawl result is return as a list
+
+	return render(request, 'portal/crawlpage')
