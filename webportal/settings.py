d59c879d43475952c493886c26d04451905d7c4a
EchoHowardLam <lamyipfu@gmail.com>
2019-03-22 19:34:34 +0800

    rebase frontend

--- webportal/settings.py
+++ webportal/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'portal',
     'users',
+    'netgrep',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'background_task',
 ]
 
 MIDDLEWARE = [
