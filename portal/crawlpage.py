bc798f13d2a92311cee2a4b7d66e5a57773c4f63
Joe Leung <joeleung00@gmail.com>
2019-03-23 13:46:56 +0800

    fix the html form and url

--- portal/crawlpage.py
+++ portal/crawlpage.py
@@ -14,10 +14,12 @@ from urllib.request import urlopen
 def crawlpage(request):
 	content = {}
 	if request.user.is_authenticated:
-	#logic for crawling
-		html = urlopen({{crawllink}}).read().decode('utf-8') 
-		soup = BeautifulSoup(html ,'html.parser')
-		element = soup.select({{crawltag}})
+		if "crawllink" in request.POST:
+			print(request.POST["crawllink"])
+			#logic for crawling
+			#html = urlopen({{crawllink}}).read().decode('utf-8')
+			#soup = BeautifulSoup(html ,'html.parser')
+			#element = soup.select({{crawltag}})
 
 	# hardcode version:
 	# html = urlopen("http://course.cse.cuhk.edu.hk/~csci3100/").read().decode('utf-8')
@@ -27,4 +29,4 @@ def crawlpage(request):
 
 	#crawl result is return as a list
 
-	return render(request, 'portal/crawlpage')
+	return render(request, 'portal/crawlpage.html')
--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from . import crawlpage
 
 urlpatterns = [
     path('category/<int:pk>/', views.category, name='portal-category'),
     path('', views.home, name='portal-home'),
     path('about/', views.about, name='portal-about'),
+    path("crawl/", crawlpage.crawlpage, name='crawl')
 ]
