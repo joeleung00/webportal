d69686760faebb24c6b0227f59af616160653b56
phleung24 <hoshisora20@gmail.com>
2019-04-11 22:55:25 +0800

    update layout, delete un-used images

--- portal/static/assets/js/scripts.js
+++ portal/static/assets/js/scripts.js
@@ -1,13 +1,6 @@
 (function(window, undefined) {
   'use strict';
 
-  /*
-  NOTE:
-  ------
-  PLACE HERE YOUR OWN JAVASCRIPT CODE IF NEEDED
-  WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR JAVASCRIPT CODE PLEASE CONSIDER WRITING YOUR SCRIPT HERE.  */
-
-
 })(window);
 
 var selectedMessage = [];
