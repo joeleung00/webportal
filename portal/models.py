4c4f96821c6d62ff27fc2900e1958dae5ba00726
Joe Leung <joeleung00@gmail.com>
2019-04-11 10:41:38 +0800

    create default category News

--- portal/models.py
+++ portal/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
@@ -38,3 +40,8 @@ class GrepRequest(models.Model):
 
     def __str__(self):
         return self.content_title
+
+@receiver(post_save, sender=User)
+def create_user_category(sender, instance, created, **kwargs):
+    if created:
+        Category.objects.create(author=instance, title="News")
